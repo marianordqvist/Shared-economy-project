10514d9d90263b41acafd421e4d3ea9e7f23cd08
marianordqvist <marianordqvist@live.se>
2024-05-22 09:50:34 +0200

    add: header navigation component

--- src/app/_components/Header/Header.tsx
+++ src/app/_components/Header/Header.tsx
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 import { menuItemsData } from "./menuItemsData";
 import Logo from "../Logo";
-import MenuItems from "./MenuItems";
+import Navigation from "./Navigation";
 
 const Header = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
@@ -21,7 +21,7 @@ const Header = () => {
         <nav>
           <ul className="flex h-dvh flex-col items-center justify-center gap-10 text-lg text-white">
             {menuItemsData.map((menu, index) => {
-              return <MenuItems items={menu} key={index} />;
+              return <Navigation items={menu} key={index} />;
             })}
           </ul>
         </nav>
@@ -36,7 +36,7 @@ const Header = () => {
         <nav>
           <ul className="hidden gap-16 sm:flex">
             {menuItemsData.map((menu, index) => {
-              return <MenuItems items={menu} key={index} />;
+              return <Navigation items={menu} key={index} />;
             })}
           </ul>
         </nav>
