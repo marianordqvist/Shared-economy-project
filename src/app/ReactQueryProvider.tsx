37866782fcc6289d9082ffdb8d82b2135a00906c
marianordqvist <marianordqvist@live.se>
2024-06-08 11:01:20 +0200

    add: ReactQueryProvider

---
+++ src/app/ReactQueryProvider.tsx
@@ -0,0 +1,13 @@
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+export const ReactQueryProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./_ui/globals.css";
 import Header from "./_components/Header/Header";
+import { ReactQueryProvider } from "./ReactQueryProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,11 +17,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Header />
-        <main>{children}</main>
-      </body>
-    </html>
+    <ReactQueryProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <Header />
+          <main>{children}</main>
+        </body>
+      </html>
+    </ReactQueryProvider>
   );
 }
