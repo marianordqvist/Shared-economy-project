dd29696eb5f03bccc585fc71fd0d01d094421bf0
marianordqvist <marianordqvist@live.se>
2024-06-10 15:06:56 +0200

    add:useFetchCosts

---
+++ src/app/hooks/useFetchCosts.js
@@ -0,0 +1,23 @@
+import { useQuery } from "@tanstack/react-query";
+
+// fetch from local storage
+const fetchLocalStorage = () => {
+  return JSON.parse(localStorage.getItem("costs") || "[]");
+};
+
+// Future fetch function to fetch from backend API
+
+// const fetchCostsFromBackend = async () => {
+//   const response = await fetch('https://your-backend-api.com/costs'); // Replace with your backend API endpoint
+//   if (!response.ok) {
+//     throw new Error('Network response was not ok');
+//   }
+//   return response.json();
+// };
+
+export const useFetchCosts = () => {
+  return useQuery("costs", fetchLocalStorage);
+
+  // To switch to backend, use:
+  // return useQuery('costs', fetchCostsFromBackend);
+};
