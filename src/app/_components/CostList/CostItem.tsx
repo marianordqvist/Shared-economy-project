95c47af47843915aace407e2da3e88985abc0409
marianordqvist <marianordqvist@live.se>
2024-06-13 09:33:25 +0200

    add category

--- src/app/_components/CostList/CostItem.tsx
+++ src/app/_components/CostList/CostItem.tsx
@@ -17,7 +17,7 @@ const CostItem: React.FC<CostItemProps> = ({
 }) => {
   return (
     <li
-      className={`${categoryColors[cost.CostCategory] || "bg-zinc-200"} ${listType ? "w-full" : "min-w-40 max-w-40"} rounded-lg px-5 py-3`}
+      className={`${categoryColors[cost.CostCategory] || "bg-zinc-200"} ${listType ? "w-full" : "max-w-40"} pointer-events-none rounded-lg px-5 py-3`}
     >
       <p className="person inline-block font-bold">{cost.PersonCategory}</p>{" "}
       bought{" "}
@@ -25,6 +25,7 @@ const CostItem: React.FC<CostItemProps> = ({
         {cost.CostDescription}{" "}
       </p>{" "}
       for <div className="cost inline-block font-bold">{cost.Cost}</div>
+      <p className="pt-2 text-xs italic">{cost.CostCategory}</p>
     </li>
   );
 };
