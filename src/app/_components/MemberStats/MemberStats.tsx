e42bf4b7f64481ec6e7b8d929eae92b928e3714c
marianordqvist <marianordqvist@live.se>
2024-06-14 10:28:32 +0200

    add: percentage/person

--- src/app/_components/MemberStats/MemberStats.tsx
+++ src/app/_components/MemberStats/MemberStats.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { useFetchCosts } from "@/app/hooks/useFetchCosts";
-import { calculateTotal, calculatePersonTotal } from "../../Utils/CostUtils";
+import {
+  calculateTotal,
+  calculatePersonTotal,
+  calculatePersonPercentage,
+} from "../../Utils/CostUtils";
 import { useState, useEffect } from "react";
 
 const MemberStats = () => {
@@ -9,13 +13,24 @@ const MemberStats = () => {
   const [totalCostPerPerson, setTotalCostPerPerson] = useState<
     { person: string; totalCost: number }[]
   >([]);
+  const [percentagePerPerson, setPercentagePerPerson] = useState<
+    {
+      person: string;
+      percentage: number;
+    }[]
+  >([]);
 
   useEffect(() => {
     if (data) {
       const total = calculateTotal(data);
       const totalPerPerson = calculatePersonTotal(data);
+      const percentagePerPerson = calculatePersonPercentage(
+        total,
+        totalPerPerson,
+      );
       setTotalCost(total);
       setTotalCostPerPerson(totalPerPerson);
+      setPercentagePerPerson(percentagePerPerson);
     }
   }, [data]);
 
@@ -25,15 +40,42 @@ const MemberStats = () => {
   return (
     <>
       <div>
-        <h2>Total cost: {totalCost}</h2>
-        <h2>Total cost per person:</h2>
-        <ul>
-          {totalCostPerPerson.map((item) => (
-            <li key={item.person}>
-              {item.person}: {item.totalCost}
-            </li>
-          ))}
-        </ul>
+        <h2 className="mb-4">Member stats</h2>
+        {/* total cost */}
+        <div className="total mb-10">
+          <h3>Total cost: {totalCost} SEK</h3>
+          {/* total cost per person */}
+        </div>
+        <div className="total-person mb-10">
+          <ul>
+            {totalCostPerPerson.map((item) => (
+              <li key={item.person} className="mb-3">
+                <div className="inline-block font-bold">{item.person}</div>
+                <br />
+                pays{" "}
+                <div className="inline-block font-bold">
+                  {item.totalCost}
+                </div>{" "}
+                sek
+              </li>
+            ))}
+          </ul>
+        </div>
+        {/* percentage per person */}
+        <div className="percentage mb-5">
+          <ul>
+            {percentagePerPerson.map((item) => (
+              <li key={item.person} className="mb-3">
+                <div className="inline-block font-bold">{item.person}</div>
+                <br />
+                pays{" "}
+                <div className="inline-block font-bold">
+                  {item.percentage} %
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </>
   );
