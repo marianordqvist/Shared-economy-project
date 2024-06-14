ab8b20f53883e9ed7596d1118390eacf7cfc5a1b
marianordqvist <marianordqvist@live.se>
2024-06-14 10:27:29 +0200

    add: percentage per person

--- src/app/Utils/CostUtils.tsx
+++ src/app/Utils/CostUtils.tsx
@@ -1,9 +1,7 @@
-import { useFetchCosts } from "../hooks/useFetchCosts";
-
 interface CostDataTypes {
   PersonCategory: string;
   CostCategory: string;
-  Cost: string;
+  Cost: number;
   CostDescription: string;
 }
 
@@ -26,9 +24,20 @@ export const calculatePersonTotal = (data: CostDataTypes[]) => {
     }
     personTotals[cost.PersonCategory] += Number(cost.Cost);
   });
-
   return Object.keys(personTotals).map((person) => ({
     person,
     totalCost: personTotals[person],
+  }));
+};
+
+// calculate percentage per person
+
+export const calculatePersonPercentage = (
+  total: number,
+  personTotals: { person: string; totalCost: number }[],
+) => {
+  return personTotals.map((personTotal) => ({
+    person: personTotal.person,
+    percentage: Math.round((personTotal.totalCost / total) * 100),
   }));
 };
