adf76e869cc3f337f69152e2faf0d8a638f107dc
marianordqvist <marianordqvist@live.se>
2024-06-26 09:22:24 +0200

    add: className props option to primary button

--- src/app/_components/PrimaryButton.tsx
+++ src/app/_components/PrimaryButton.tsx
@@ -1,26 +1,29 @@
 import React from "react";
 
+interface PrimaryButtonProps {
+  buttonText: string;
+  onClick?: () => void; // Optional onClick prop
+  className?: string; // Ooptional styling props
+}
+
 /**
  * The PrimaryButton accepts the button's text as a string.
  *
  * @param {string} [options.buttonText] - The name of the button.
  * @param {function} [onClick] - The function to call when the button is clicked.
+ * @param {string} [options.className] - Option to pass more styling to the button
  * @returns {JSX.Element} The button component.
  *
  */
 
-interface PrimaryButtonProps {
-  buttonText: string;
-  onClick?: () => void; // Optional onClick prop
-}
-
 const PrimaryButton: React.FC<PrimaryButtonProps> = ({
   buttonText,
   onClick,
+  className,
 }) => {
   return (
     <button
-      className="bg-pink hover:bg-hotpink min-w-16 rounded-xl px-3 py-2 text-sm text-black hover:text-white"
+      className={`${className}min-w-16 rounded-xl bg-pink px-3 py-2 text-sm text-black hover:bg-hotpink hover:text-white`}
       type="submit"
       onClick={onClick}
     >
