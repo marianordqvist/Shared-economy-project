59b603a8e634e36c03175b3a29111a29b84c8415
marianordqvist <marianordqvist@live.se>
2024-06-28 13:39:45 +0200

    add: more colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,24 @@ module.exports = {
     extend: {
       colors: {
         orange: "#F6BD60",
+        darkOrange: "#7d5b23",
+        lightOrange: "#f6cb87",
         blue: "#D2DCE6",
+        darkBlue: "#2e7bc7",
+        lightBlue: "#d1e2f4",
         pink: "#F5CAC3",
+        darkPink: "#854238",
+        lightPink: "#dcc0bb",
         green: "#B3D1CA",
+        darkGreen: "#466255",
         hotpink: "#F28482",
-        blue: "#D2DCE6",
+        darkHotpink: "#6d1918",
       },
     },
   },
   plugins: [],
 };
+
+// function color() {
+//   <div className="bg-[#6d1918]"></div>;
+// }
